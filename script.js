2dd390e26c8b76eb3fbcdecb62ef537818eb0623
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-08-21 12:08:19 +0200

    done the checkpoint js

---
+++ script.js
@@ -0,0 +1,23 @@
+let divINfo = document.getElementById("pokeInfo");
+let input = document.getElementById("pokename");
+
+let btn = document.getElementById("search");
+let p1 = document.getElementById("exp");
+let moves = document.getElementById("moves");
+let image = document.getElementById("imgPoke");
+btn.addEventListener("click", () => {
+  getInfo();
+});
+
+function getInfo() {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${input.value}`)
+    .then((res) => res.json())
+    .then((json) => json)
+    .then((res) => {
+      console.log(res.name);
+      res.abilities.forEach((el) => (moves.innerText = el.ability.name));
+      exp.innerText = res.base_experience;
+      image.src = res.sprites.front_default;
+    })
+    .catch((err) => console.log(err));
+}
